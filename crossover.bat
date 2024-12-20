407b3a4c2a33ad76e6c133e00de4fd2ac8f101ff
Zach Reid <75558285+zachreid-96@users.noreply.github.com>
2024-12-19 22:26:55 -0600

    Update crossover.bat
    updated version number in script

--- crossover.bat
+++ crossover.bat
@@ -33,7 +33,7 @@ if '%errorlevel%' NEQ '0' (
 
 :: Stylistic section of the program, turns echo off so these commands do not appear in CMD
 @echo off
-set version=3.1.2
+set version=3.1.4
 title IPv4 Copier Cross-Over by Zach (v%version%)
 setlocal EnableExtensions EnableDelayedExpansion
 color B
